5eb12db4790ccabfb6cde86c54066b3103f50238
Elliesshard <josesavio2008@gmail.com>
2025-10-21 16:25:22 -0300

    Update which.js

--- scripts/which.js
+++ scripts/which.js
@@ -1,8 +1,9 @@
-let wit = prompt("What is your name?");
+let wit = prompt("What is it?");
 
 if (wit !== "Determination"){
 alert(`ERRADO!`);
 window.location.href="index.html"
 } else {
 alert(`"7"`);
+
 }
