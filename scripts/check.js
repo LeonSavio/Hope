8939da6daf0c746e237b7fff14276d5cffe48b2e
Elliesshard <josesavio2008@gmail.com>
2025-11-13 19:08:53 -0300

    polaris

---
+++ scripts/check.js
@@ -0,0 +1,9 @@
+  document.getElementById("Check").onclick = function() {
+  const Page =(window.location.search);
+
+  // Verifica o valor e redireciona
+  if (Page == "?elliesshard") {
+    window.location.href = " #";
+  }
+
+};
