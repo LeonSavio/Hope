2a9ecf69abee5deda3c93f31430ce409927a084d
Elliesshard <josesavio2008@gmail.com>
2025-11-13 23:55:48 -0300

    test

--- scripts/check.js
+++ scripts/check.js
@@ -1,7 +1,12 @@
+document.getElementById("Check").onclick = function() {
+  // Pega os parâmetros da URL
+  const params = new URLSearchParams(window.location.search);
 
-  var Page = window.location.search 
-  alert(Page)
+  // Obtém o valor do parâmetro "Page"
+  const Page = params.get("Page");
 
-  if (Page == "?elliesshard"){ 
-    window.location.href = '61.html' 
-  } 
+  // Verifica o valor e redireciona
+  if (Page === "elliesshard") {
+    window.location.href = "61.html";
+  }
+};
