cb707f7a18b9937da7d4e6e323b17a5811ae1311
Elliesshard <josesavio2008@gmail.com>
2025-11-13 23:53:20 -0300

    .

--- scripts/check.js
+++ scripts/check.js
@@ -1,7 +1,7 @@
-document.getElementById("Check").onclick = function() { 
+
   var Page = window.location.search 
   alert(Page)
 
   if (Page == "?elliesshard"){ 
     window.location.href = '61.html' 
-  } };
+  } 
